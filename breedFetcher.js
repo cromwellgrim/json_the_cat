5c3af37a6824e88b714753fae0e2cf4586987888
cromwellgrim <rogers.dylan@protonmail.com>
2021-02-11 23:52:06 +0000

    updates

--- breedFetcher.js
+++ breedFetcher.js
@@ -2,28 +2,28 @@ const request = require('request');
 
 let args = process.argv.slice(2);
 let breedName = args[0];
-let url = 'https://api.thecatapi.com/v1/breeds/search?q=' + breedName;
 
-request(url, function(error, response, body) {
-  if (error !== null) {
-    console.log('error: ', error);
-  }
-  // console.log('body: ', body);
-  // console.log('response: ', response.statusCode);
-  
-  const data = JSON.parse(body);
-  // console.log(data);
-  if (data.length === 0) {
-    console.log('no such breed');
-  }
-  let dataArray = Object.entries(data);
-  dataArray.forEach(([key, value]) => {
-    // console.log(key);
-    console.log(value.description);
-    return;
+
+const fetchBreedDescription = function(breedName, callback) {
+
+  let desc;
+  let url = 'https://api.thecatapi.com/v1/breeds/search?q=' + breedName;
+
+  request(url, function(error, response, body) {
+    const data = JSON.parse(body);
+
+    if (data.length === 0) {
+      callback('breed not found', null);
+      return;
+    }
+
+    desc = data[0].description;
+
+    callback(null, desc);
+    
   });
+  return desc;
+};
 
 
-  return;
-});
-
+module.exports = { fetchBreedDescription };
